ec5f34e5389112c2b71a92f1d8bd03d0ef29da11
kaka <vn503024@gmail.com>
2022-02-11 15:15:10 +0800

    feat: added `vis_intensity_channel` and `print_pitch_resolution`

--- lesson-1-lidar-sensor/exercises/starter/l1_exercises.py
+++ lesson-1-lidar-sensor/exercises/starter/l1_exercises.py
@@ -29,23 +29,56 @@ from tools.waymo_reader.simple_waymo_open_dataset_reader import dataset_pb2
 def vis_intensity_channel(frame, lidar_name):
 
     print("Exercise C1-5-5")
-    # extract range image from frame
 
-    # map value range to 8bit
+    # extract range image from frame
+    lidar = [obj for obj in frame.lasers if obj.name == lidar_name][0] # get lasers data structure from frame
+    ri = [] # ri: ramge_image
+    if len(lidar.ri_return1.range_image_compressed) > 0: # use first response
+        ri = dataset_pb2.MatrixFloat()
+        ri.ParseFromString(zlib.decompress(lidar.ri_return1.range_image_compressed))
+        ri = np.array(ri.data).reshape(ri.shape.dims)
+    ri[ri<0]=0.0
+
+    # map value intensity to 8bit
+    ri_intensity = ri[:,:,1]
+    #ri_intensity = ri_intensity * 255 / (np.amax(ri_intensity) - np.amin(ri_intensity))
+    # contrast adjustment
+    ri_intensity = (np.amax(ri_intensity) / 2) * (ri_intensity * 255 / (np.amax(ri_intensity) - np.amin(ri_intensity)))
+    img_intensity = ri_intensity.astype(np.uint8)
 
     # focus on +/- 45° around the image center
+    deg45 = int(img_intensity.shape[1] / 8) # 360° / 45° = 8
+    ri_center = int(img_intensity.shape[1]/2)
+    img_intensity = img_intensity[:,ri_center-deg45:ri_center+deg45]
 
+    cv2.imshow('intensity image', img_intensity)
+    cv2.waitKey(0)
 
 
 # Exercise C1-5-2 : Compute pitch angle resolution
 def print_pitch_resolution(frame, lidar_name):
 
     print("Exercise C1-5-2")
-    # load range image
 
-    # compute vertical field-of-view from lidar calibration
+    # 1. Load range image
+    lidar = [obj for obj in frame.lasers if obj.name == lidar_name][0] # get lasers data structure from frame
+    ri = [] # ri: ramge_image
+    if len(lidar.ri_return1.range_image_compressed) > 0: # use first response
+        ri = dataset_pb2.MatrixFloat()
+        ri.ParseFromString(zlib.decompress(lidar.ri_return1.range_image_compressed))
+        ri = np.array(ri.data).reshape(ri.shape.dims)
+
+    # 2. Compute vertical field-of-view (VFOV) from lidar calibration in rad
+    lidar_calib = [obj for obj in frame.context.laser_calibrations if obj.name == lidar_name][0] # get laser calibration
+    min_pitch = lidar_calib.beam_inclination_min
+    max_pitch = lidar_calib.beam_inclination_max
+    vfov_rad = max_pitch - min_pitch
 
     # compute pitch resolution and convert it to angular minutes
+    ## ri: row is pitcn
+    pitch_resolution_rad = vfov_rad / ri.shape[0]
+    pitch_resolution_deg = pitch_resolution_rad * 180 / np.pi
+    print("pitch angle resolution = " + '{0:.2f}'.format(pitch_resolution_deg) + "°")
 
 
 # Exercise C1-3-1 : print no. of vehicles
