7aecc73c130ec5d3fc57815e88e52085b71f2908
kaka <vn503024@gmail.com>
2022-02-11 15:12:14 +0800

    chore: update dockerfile and script for can run OpenGL in Container

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ docker run \
     -it \
     --gpus all \
     -e DISPLAY=$DISPLAY \
+	-e QT_X11_NO_MITSHM=1 \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix" \
     --network=host \
     --shm-size="20g" \
     --volume="$PWD:/app/project/" \
