834aa603a8f188b91420c7bc93b1868bd1594edc
kaka <vn503024@gmail.com>
2022-03-14 18:52:11 +0800

    feat: completed `EKF, predict update`

--- lesson-3-EKF/exercises/starter/1_predict_update.py
+++ lesson-3-EKF/exercises/starter/1_predict_update.py
@@ -14,18 +14,24 @@ class Filter:
         # process noise covariance Q
         return np.matrix([[0, 0],
                         [0, 0]])
-        
+
     def H(self):
         # measurement matrix H
         return np.matrix([[1, 0]])
-    
+
     def predict(self, x, P):
         # predict state and estimation error covariance to next timestep
 
         ############
         # TODO: implement prediction step
         ############
-        
+
+        F = self.F()
+        Q = self.Q()
+
+        x = F * x # state prediction
+        P = F * P * F.transpose() + Q # covariance prediction
+
         return x, P
 
     def update(self, x, P, z, R):
@@ -34,44 +40,53 @@ class Filter:
         ############
         # TODO: implement update step
         ############
-        
-        return x, P     
-        
-        
+
+        H = self.H()
+        I = np.identity(self.dim_state)
+
+        gamma = z - H * x # measurement residual, gamma
+        S = H * P * H.transpose() + R # covariance of residual
+        K = P * H.transpose() * np.linalg.inv(S) # Kalman gain
+        x = x + K * gamma # state update
+        P = (I - K * H) * P # covariance update
+
+        return x, P
+
+
 def run_filter():
     ''' loop over data and call predict and update'''
     np.random.seed(10) # make random values predictable
-    
+
     # init filter
     KF = Filter()
-    
+
     # init track state and covariance
     x = np.matrix([[0],
                 [0]])
     P = np.matrix([[5**2, 0],
-                [0, 5**2]])  
-    
+                [0, 5**2]])
+
     # loop over measurements and call predict and update
-    for i in range(1,101):        
+    for i in range(1,101):
         print('------------------------------')
         print('processing measurement #' + str(i))
-        
+
         # prediction
         x, P = KF.predict(x, P) # predict to next timestep
         print('x- =', x)
         print('P- =', P)
-        
+
         # measurement generation
         sigma_z = 1 # measurement noise
         z = np.matrix([[i + np.random.normal(0, sigma_z)]]) # generate noisy measurement
         R = np.matrix([[sigma_z**2]]) # measurement covariance
         print('z =', z)
-        
+
         # update
         x, P = KF.update(x, P, z, R) # update with measurement
         print('x+ =', x)
         print('P+ =', P)
-        
+
 
 # call main loop
 run_filter()
