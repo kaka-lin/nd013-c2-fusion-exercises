9983f03cb18dd36167fe4aa98291cbab624c8c90
kaka <vn503024@gmail.com>
2022-03-15 18:01:38 +0800

    feat: completed `lesson-3-EKF, 2_filter and 3_measurements`

--- lesson-3-EKF/exercises/starter/2_filter.py
+++ lesson-3-EKF/exercises/starter/2_filter.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib
-matplotlib.use('wxagg') # change backend so that figure maximizing works on Mac as well  
+from torch import quantized_max_pool1d
+matplotlib.use('wxagg') # change backend so that figure maximizing works on Mac as well
 import matplotlib.pyplot as plt
 
 class Filter:
@@ -16,8 +17,12 @@ class Filter:
         ############
         # TODO: implement and return F
         ############
-        
-        pass
+
+        dt = self.dt
+        return np.matrix([[1, 0, dt, 0],
+                          [0, 1, 0, dt],
+                          [0, 0, 1, 0],
+                          [0, 0, 0, 1]])
 
     def Q(self):
         # process noise covariance Q
@@ -25,18 +30,24 @@ class Filter:
         ############
         # TODO: implement and return Q
         ############
-        
-        pass
-    
+        dt = self.dt
+        q = self.q
+        dt_square, dt_cube = np.power(dt, 2), np.power(dt, 3)
+        return np.matrix([[(dt_cube * q) / 3, 0, (dt_square * q) / 2, 0],
+                          [0, (dt_cube * q) / 3, 0, (dt_square * q) / 2],
+                          [(dt_square * q) / 2, 0, dt * q, 0],
+                          [0, (dt_square * q) / 2, 0,  dt * q]])
+
     def H(self):
         # measurement matrix H
 
         ############
         # TODO: implement and return H
         ############
-    
-        pass
-    
+
+        return np.matrix([[1, 0, 0, 0],
+                          [0, 1, 0, 0]])
+
     def predict(self, x, P):
         # predict state and estimation error covariance to next timestep
         F = self.F()
@@ -53,19 +64,19 @@ class Filter:
         x = x + K*gamma # state update
         I = np.identity(self.dim_state)
         P = (I - K*H) * P # covariance update
-        return x, P   
-        
-        
+        return x, P
+
+
 def run_filter():
     ''' loop over data and call predict and update'''
     np.random.seed(0) # make random values predictable
-    
+
     # init filter
     KF = Filter()
-    
+
     # init figure
     fig, ax = plt.subplots()
-    
+
     # init track state and covariance
     x = np.matrix([[0],
                 [0],
@@ -75,28 +86,28 @@ def run_filter():
                 [0, 0.1**2, 0, 0],
                 [0, 0, 2**2, 0],
                 [0, 0, 0, 2**2]])
-    
+
     # loop over measurements and call predict and update
-    for i in range(1,101):        
-        
+    for i in range(1,101):
+
         # prediction
         x, P = KF.predict(x, P) # predict to next timestep
-        
+
         # ground truth generation
-        gt = np.matrix([[i*KF.dt], 
+        gt = np.matrix([[i*KF.dt],
                        [0.1*(i*KF.dt)**2]])
-        
+
         # measurement generation
-        sigma_z = 0.2 # measurement noise 
+        sigma_z = 0.2 # measurement noise
         z = np.matrix([[float(gt[0]) + np.random.normal(0, sigma_z)],
                        [float(gt[1]) + np.random.normal(0, sigma_z)]]) # generate noisy measurement
         R = np.matrix([[sigma_z**2, 0], # measurement noise covariance matrix
                             [0, sigma_z**2]])
-        
+
         # update
         x, P = KF.update(x, P, z, R) # update with measurement
-        
-        # visualization    
+
+        # visualization
         ax.scatter(float(x[0]), float(x[1]), color='green', s=40, marker='x', label='track')
         ax.scatter(float(z[0]), float(z[1]), color='blue', marker='.', label='measurement')
         ax.scatter(float(gt[0]), float(gt[1]), color='gray', s=40, marker='+', label='ground truth')
@@ -104,11 +115,11 @@ def run_filter():
         ax.set_ylabel('y [m]')
         ax.set_xlim(0,10)
         ax.set_ylim(0,10)
-           
-        # maximize window        
+
+        # maximize window
         mng = plt.get_current_fig_manager()
-        mng.frame.Maximize(True) 
-        
+        mng.frame.Maximize(True)
+
         # remove repeated labels
         handles, labels = ax.get_legend_handles_labels()
         handle_list, label_list = [], []
@@ -120,7 +131,7 @@ def run_filter():
 
         plt.pause(0.01)
     plt.show()
-        
+
 
 ####################
 # call main loop
--- lesson-3-EKF/exercises/starter/3_measurements.py
+++ lesson-3-EKF/exercises/starter/3_measurements.py
@@ -1,7 +1,7 @@
 # imports
 import numpy as np
 import matplotlib
-matplotlib.use('wxagg') # change backend so that figure maximizing works on Mac as well  
+matplotlib.use('wxagg') # change backend so that figure maximizing works on Mac as well
 import matplotlib.pyplot as plt
 
 class Camera:
@@ -10,29 +10,51 @@ class Camera:
         self.f_i = 2095.5 # focal length i-coordinate
         self.f_j = 2095.5 # focal length j-coordinate
         self.c_i = 944.9 # principal point i-coordinate
-        self.c_j = 640.2 # principal point j-coordinate 
-        
-    def get_hx(self, x):    
-        # calculate nonlinear measurement expectation value h(x)   
+        self.c_j = 640.2 # principal point j-coordinate
+
+    def get_hx(self, x):
+        # calculate nonlinear measurement expectation value h(x)
         hx = np.zeros((2,1))
 
         ############
         # TODO: implement and return h(x)
         ############
-        
+        fi = self.f_i
+        fj = self.f_j
+        ci = self.c_i
+        cj = self.c_j
+
+        if x[0]==0:
+            raise NameError('Jacobian not defined for x[0]=0!')
+        else:
+            hx[0, 0] = ci - fi * x[1] / x[0]
+            hx[1, 0] = cj - fj * x[2] / x[0]
+
         return hx
-    
+
     def get_H(self, x):
         # calculate Jacobian H at current x from h(x)
         H = np.matrix(np.zeros((2, 6)))
 
         ############
         # TODO: implement and return H
-        ############ 
-        
+        ############
+        fi = self.f_i
+        fj = self.f_j
+        ci = self.c_i
+        cj = self.c_j
+
+        if x[0]==0:
+            raise NameError('Jacobian not defined for x[0]=0!')
+        else:
+            H[0, 0] = fi * x[1] / (x[0] ** 2)
+            H[0, 1] = -(fi / x[0])
+            H[1, 0] = fj * x[2] / (x[0] ** 2)
+            H[1, 2] = -(fj / x[0])
+
         return H
- 
- 
+
+
 def calc_Jacobian(x):
     # calculate Jacobian for x
     cam = Camera()
@@ -51,7 +73,7 @@ def calc_Jacobian(x):
     ax1.plot(x[0], hx_orig[0], marker='x', color='green', label='expansion point x')
     ax2.plot(x[0], hx_orig[1], marker='x', color='green', label='expansion point x')
 
-    # calculate linear approximation at this point 
+    # calculate linear approximation at this point
     s1 = float(H[0,0]) # slope of tangent given by Jacobian H
     s2 = float(H[1,0])
     i1 = float(hx_orig[0] - s1*x[0]) # intercept i = y - s*x
@@ -66,14 +88,14 @@ def calc_Jacobian(x):
         plot_y2.append(hx[1])
         lin_y1.append(s1*px + i1)
         lin_y2.append(s2*px + i2)
-        
+
     # plot results
     ax1.plot(plot_x, plot_y1, color='blue', label='measurement function h')
     ax1.plot(plot_x, lin_y1, color='red', label='linear approximation H')
     ax2.plot(plot_x, plot_y2, color='blue', label='measurement function h')
     ax2.plot(plot_x, lin_y2, color='red', label='linear approximation H')
 
-    # maximize window     
+    # maximize window
     mng = plt.get_current_fig_manager()
     mng.frame.Maximize(True)
 
